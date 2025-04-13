f3fd94150fda858e5f6c6feac8aa581dedf445bc
ALISA <146487164+allfeia@users.noreply.github.com>
2025-04-13 23:40:00 +0300

    Update svn_repo.sh

--- svn_repo.sh
+++ svn_repo.sh
@@ -8,78 +8,80 @@ cd ..
 svn checkout $REPO_URL/trunk working_copy
 cd working_copy
 
-cp -r ../commits/commit0 . 
-svn add commit0
+cp -r ../commits/commit0/* . 
+svn add .
 svn commit -m "commit0 (red)" --username Tralalelo --password krokodilo
 
-cp -r ../commits/commit1 .
-svn add commit1
+cp -r ../commits/commit1/* .
+svn add .
 svn commit -m "commit1 (red)" --username Tralalelo --password krokodilo
 
 svn copy $REPO_URL/trunk $REPO_URL/branches/branch2 -m "creating branch2"
 svn switch $REPO_URL/branches/branch2
-cp -r ../commits/commit2 .
-svn add commit2
+cp -r ../commits/commit2/* .
+svn add .
 svn commit -m "commit2 (blue)" --username Trulala --password pepperoni
 
-cp -r ../commits/commit3 .
-svn add commit3
+cp -r ../commits/commit3/* .
+svn add .
 svn commit -m "commit3 (blue)" --username Trulala --password pepperoni
 
-cp -r ../commits/commit4 .
-svn add commit4
+cp -r ../commits/commit4/* .
+svn add .
 svn commit -m "commit4 (blue)" --username Trulala --password pepperoni
 
 svn copy $REPO_URL/branches/branch2 $REPO_URL/branches/branch3 -m "creating branch3"
 svn switch $REPO_URL/branches/branch3 
-cp -r ../commits/commit5 .
-svn add commit5
+cp -r ../commits/commit5/* .
+svn add .
 svn commit -m "commit5 (blue)" --username Trulala --password pepperoni             
 
 svn switch $REPO_URL/trunk
-cp -r ../commits/commit6 .
-svn add commit6
+cp -r ../commits/commit6/* .
+svn add .
 svn commit -m "commit6 (red)" --username Tralalelo --password krokodilo  
 
 svn switch $REPO_URL/branches/branch2
-cp -r ../commits/commit7 .
-svn add commit7
+cp -r ../commits/commit7/* .
+svn add .
 svn commit -m "commit7 (blue)" --username Trulala --password pepperoni
 
 svn switch $REPO_URL/trunk 
-cp -r ../commits/commit8 .
-svn add commit8
+cp -r ../commits/commit8/* .
+svn add .
 svn commit -m "commit8 (red)" --username Tralalelo --password krokodilo
 
 svn switch $REPO_URL/branches/branch2
-cp -r ../commits/commit9 .
-svn add commit9
+cp -r ../commits/commit9/* .
+svn add .
 svn commit -m "commit9 (blue)" --username Trulala --password pepperoni
 
 svn switch $REPO_URL/trunk     
-cp -r ../commits/commit10 .
-svn add commit10
+cp -r ../commits/commit10/* .
+svn add .
 svn commit -m "commit10 (red)" --username Tralalelo --password krokodilo
 
 svn switch $REPO_URL/branches/branch3
-cp -r ../commits/commit11 .
-svn add commit11 
+cp -r ../commits/commit11/* .
+svn add . 
 svn commit -m "commit11 (blue)" --username Trulala --password pepperoni
 
 svn switch $REPO_URL/branches/branch2
-cp -r ../commits/commit12 .
-svn add commit12 
+svn merge $REPO_URL/branches/branch3
+svn commit -m "merger branch3 into branch2" --username Tralalelo --password krokodilo
+cp -r ../commits/commit12/* .
+svn add . 
 svn commit -m "commit12 (blue)" --username Trulala --password pepperoni
 
-cp -r ../commits/commit13 .
-svn add commit13 
+cp -r ../commits/commit13/* .
+svn add . 
 svn commit -m "commit13 (blue)" --username Trulala --password pepperoni
 
 svn switch $REPO_URL/trunk     
 svn merge $REPO_URL/branches/branch2
 svn commit -m "merger branch2 into trunk" --username Tralalelo --password krokodilo
-cp -r ../commits/commit14 .
-svn add commit14
+cp -r ../commits/commit14/* .
+svn add .
 svn commit -m "commit14 (red)" --username Tralalelo --password krokodilo
 
 svn update
