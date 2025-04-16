c9f3fddb8bc3a48d4da85cea1de839f084d8ac6c
ALISA <146487164+allfeia@users.noreply.github.com>
2025-04-16 15:34:21 +0300

    Update git_repo.sh

--- git_repo.sh
+++ git_repo.sh
@@ -11,8 +11,6 @@ git commit -m "commit0 (red)"
 
 cp -r ../commits/commit1/* .
 git add .
-git config --global user.name "Tralalelo"
-git config --global user.email "tralalelo@gmail.com"
 git commit -m "commit1 (red)"
 
 git checkout -b "branch2"
@@ -24,21 +22,15 @@ git commit -m "commit2 (blue)"
 
 cp -r ../commits/commit3/* .
 git add .
-git config --global user.name "Trulala"
-git config --global user.email "trulala@gmail.com"
 git commit -m "commit3 (blue)"
 
 cp -r ../commits/commit4/* .
 git add .
-git config --global user.name "Trulala"
-git config --global user.email "trulala@gmail.com"
 git commit -m "commit4 (blue)"
 
 git checkout -b "branch3"
 cp -r ../commits/commit5/* .
 git add .
-git config --global user.name "Trulala"
-git config --global user.email "trulala@gmail.com"
 git commit -m "commit5 (blue)"
 
 git checkout "master"
@@ -87,14 +79,10 @@ git checkout "branch2"
 git merge "branch3" --no-commit
 cp -r ../commits/commit12/* .
 git add .
-git config --global user.name "Trulala"
-git config --global user.email "trulala@gmail.com"
 git commit -m "commit12 (blue)"
 
 cp -r ../commits/commit13/* .
 git add .
-git config --global user.name "Trulala"
-git config --global user.email "trulala@gmail.com"
 git commit -m "commit13 (blue)"
 
 git checkout "master"
